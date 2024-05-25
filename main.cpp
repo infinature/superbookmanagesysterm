b8230f1235b372979f49ba003145c61342762ee9
infinature <m1443812005@163.com>
2024-05-25 11:18:11 +0800

    实现了基础的图形界面
    下一步要添加登录窗口

--- main.cpp
+++ main.cpp
@@ -1,13 +1,58 @@
-#include<SDL2/SDL.h>
-#include<head.h>
-#include<iostream>
-using namespace std;
-int main()
-{
-    SDL_Init(SDL_INIT_VIDEO);//初始化窗口
-    SDL_Window *mainwin=SDL_CreateWindow("SuperBookManageSysterm",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,900,600,0);//创建主窗口
-    SDL_DestroyWindow(mainwin);//删除主窗口
-    SDL_Quit();//推出sdl
-    return 0;
+#include <SDL2/SDL.h>
+#include <windows.h>
 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // 你的代码，这里可以留空，因为SDL程序通常使用main作为入口点
+    return main(__argc, __argv); // 如果需要，可以调用main函数
+}
+
+int main(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        SDL_Log("Init Failed: %s", SDL_GetError());
+        return -1;
+    }//初始化视频
+    SDL_Window *win = SDL_CreateWindow("TestWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,1200, 800, 0); //创建主窗口win
+    SDL_Event event;//定义事件
+    SDL_Surface *surf=SDL_GetWindowSurface(win);//创建图形surf
+    SDL_Surface *surf2=SDL_GetWindowSurface(win);//创建图形surf
+    SDL_Surface *bmp_surf=SDL_LoadBMP("山大校徽.bmp");//导入图片
+    SDL_Surface *bmp_surf2=SDL_LoadBMP("1.bmp");//导入图片
+    SDL_Surface *bmp_surf3=SDL_LoadBMP("2.bmp");//导入图片
+    SDL_Rect rect={0,400,300,0};//定义矩形
+    SDL_Rect rect3={0,0,1200,300};//定义矩形
+    SDL_Rect rect2={0,0,1200,900};//定义矩形
+    SDL_FillRect(surf2,&rect2,SDL_MapRGB(surf->format,255,255,255));//创建矩形
+    SDL_FillRect(surf,&rect,SDL_MapRGB(surf->format,255,255,255));//创建矩形
+    SDL_FillRect(surf,&rect3,SDL_MapRGB(surf->format,255,255,255));//创建矩形
+    SDL_BlitSurface(bmp_surf2,NULL,surf2,&rect2);//显示绘图
+    SDL_BlitSurface(bmp_surf,NULL,surf2,&rect);//显示绘图
+        SDL_BlitSurface(bmp_surf3,NULL,surf,&rect3);//显示绘图
+    SDL_UpdateWindowSurface(win);//更新图形
+
+    // 登录模块
+    SDL_Window *loginWin = SDL_CreateWindow("Login", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 300, 0);
+    SDL_Surface *loginSurf = SDL_GetWindowSurface(loginWin);
+    // 在这里添加登录表单的绘制代码，如用户名、密码输入框和登录按钮等
+
+    while(true)
+    {
+        if(SDL_PollEvent(&event))
+        {
+            if(event.type==SDL_QUIT)
+            {
+                break;//直到用户关闭再关闭
+            }
+            // 在这里添加登录按钮的点击事件处理代码，用于验证用户名和密码
+        }
+
+    }
+    SDL_FreeSurface(bmp_surf);
+    SDL_FreeSurface(surf);
+    SDL_FreeSurface(surf2);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
 }
