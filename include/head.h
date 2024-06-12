6ee51f24b13f72d72ab3cab5ff5f88502ef59b75
infinature <m1443812005@163.com>
2024-06-12 21:42:13 +0800

    1
    1

--- include/head.h
+++ include/head.h
@@ -13,7 +13,12 @@
 #include <map>
 #include <list>
 #include <iomanip> 
-
+#include <SDL2/SDL.h> // 包含SDL2库的头文件，用于创建窗口和处理事件等
+#include <SDL2/SDL_ttf.h> // 包含SDL_ttf库的头文件，用于处理字体
+#include <stdexcept> // 包含标准异常类的头文件
+#include <chrono> // 包含时间日期相关的头文件
+#include <thread> // 包含线程的头文件
+#include <cmath>
 #include "Book.h"
 #include "User.h"
 #include "Index.h"
